1eff453358a9c9f311cbb445cdddbe018272c79f
Victor Cano <vikkno123@gmail.com>
2022-06-27 15:51:31 -0500

    feat: pagina principal

---
+++ red_bicicletas/public/javascripts/map.js
@@ -0,0 +1,9 @@
+var map = L.map('main_map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+L.marker([51.505, -0.09], 13).addTo(map);
+
