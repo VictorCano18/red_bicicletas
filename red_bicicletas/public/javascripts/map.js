d74874e43aa364275dae211c71b454c9c43284a3
Victor Cano <vikkno123@gmail.com>
2022-06-28 11:44:35 -0500

    map

--- red_bicicletas/public/javascripts/map.js
+++ red_bicicletas/public/javascripts/map.js
@@ -4,7 +4,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-var marker = L.marker([19.260225, -99.632754]).addTo(map);
+// let marker = L.marker([19.260225, -99.632754]).addTo(map);
+// marker = L.marker([19.260225, -98.632754]).addTo(map);
 
 
 $.ajax({
@@ -13,7 +14,7 @@ $.ajax({
     success: function(result){
         console.log(result);
         result.bicicletas.forEach(function(bici) {
-            var marker = L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
         })
     }
 })
